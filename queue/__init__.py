090c5f49d29a3a5f5c055b65b6b34ca5d935ce1a
rajboke96 <rajendra.boke08@gmail.com>
2025-11-24 10:49:50 +0530

    moved files to ds folder

--- queue/__init__.py
+++
@@ -1,4 +0,0 @@
-import sys
-sys.path.append("/home/rajendra/projects/python_proj")
-
-from ds.queue.queue_using_SLL_concept import Queue
--- stack/__init__.py
+++
@@ -1,4 +0,0 @@
-import sys
-sys.path.append("/home/rajendra/projects/python_proj")
-
-from ds.stack.stack_using_SLL_concept import Stack
