5dd75684be3b8f047503eb811fbb06e4aec915a1
rajboke96 <rajendra.boke08@gmail.com>
2025-11-24 15:16:04 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="python-collections",
+    version="1.0.0",
+    packages=find_packages(),
+    description="Python Data Structures and Algorithms implementations",
+    author="Rajendra Boke",
+    url="https://github.com/rajboke96/python-collections",
+    license="MIT",
+)
